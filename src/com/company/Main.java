f215e42ba6d239c21bc7f7ae37f1ab4eaf8786d1
Zigmas <zambrazaitis@gmail.com>
2018-01-31 15:32:04 +0200

    uzduotis3.06

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private String masyvas[];
+    private Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Main objektas = new Main();
+        objektas.nuskaitymas();
+        objektas.isvedimas();
+    }
+
+    private void nuskaitymas() {
+        masyvas = new String[5];
+        for (int i = 0; i < masyvas.length; i++) {
+            System.out.println("Iveskite " + (i + 1) + "-aji zodi: ");
+            masyvas[i] = sc.nextLine();
+        }
+    }
+
+    private void isvedimas() {
+        for (int i = 0; i < masyvas.length; i++) {
+            System.out.println((i + 1) + "-asis Jusu zodis: " + masyvas[i]);
+        }
+    }
+}
